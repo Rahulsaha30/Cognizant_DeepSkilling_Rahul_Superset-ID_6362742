4b839d2c938352b37fae996236032972b30a9c6c
Rahul Saha <rahulsaha1126@gmail.com>
2025-06-19 01:18:12 +0530

    📦 Added Singleton.cs and output screenshot in Week1 folder

---
+++ Week1/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Singleton
+{
+  //we crreate a private instance of this student class
+  private static Singleton? student;
+
+  //then we create a priavte constructor of this student class so it is not accesible 
+  private Singleton(){
+    Console.WriteLine("Student created");
+  }
+
+  public static Singleton getStudent() {
+  if (student == null)
+  {
+    student = new Singleton();
+  }
+  return student;
+  }
+
+  public void studentMessage() {
+  Console.WriteLine("HELLO");
+}
+}
+class Program
+{
+    static void Main()
+    {
+        Singleton obj1 = Singleton.getStudent();
+        obj1.studentMessage();
+
+       Singleton obj2 = Singleton.getStudent();
+        obj2.studentMessage();
+
+        Console.WriteLine(obj1 == obj2); // True ✅ Same object
+    }
+}
