b8573fcda032f8253af95af0e18706b5b93955c7
Rahul Saha <rahulsaha1126@gmail.com>
2025-06-29 15:45:24 +0530

    Updated CalcLibrary and NUnit test project

--- Week2/Code/NUNIT_AND_MOQ/UnitTest/UnitTest1.cs
+++ Week2/Code/NUNIT_AND_MOQ/UnitTest/UnitTest1.cs
@@ -1,32 +1,67 @@
 ﻿using System.Runtime.CompilerServices;
 using NUnit.Framework.Legacy;
 using UnitTest.Calc;
+using System;
 
-[TestFixture]
-public class Tests
+namespace UnitTestProject
 {
-
-    Calculator calc;
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class CalculatorTests
     {
-        calc = new Calculator();
-    }
+        private SimpleCalculator calculator;
 
-    [Test]
-    public void CalculatorTests()
-    {
-        int a = 5;
-        int b = 23;
-        int result = calc.Add(a, b);
-        ClassicAssert.AreEqual(28, result);
-    }
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new SimpleCalculator();
+        }
 
-    [TearDown]
-    public void TearDown()
-    {
-        calc=null;
+        [Test]
+        public void Addition_ReturnsCorrectSum()
+        {
+            double result = calculator.Addition(4, 5);
+            Assert.AreEqual(9, result);
+        }
+
+        [Test]
+        public void Subtraction_ReturnsCorrectDifference()
+        {
+            double result = calculator.Subtraction(10, 3);
+            Assert.AreEqual(7, result);
+        }
+
+        [Test]
+        public void Multiplication_ReturnsCorrectProduct()
+        {
+            double result = calculator.Multiplication(6, 2);
+            Assert.AreEqual(12, result);
+        }
+
+        [Test]
+        public void Division_ReturnsCorrectQuotient()
+        {
+            double result = calculator.Division(10, 2);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Division_ByZero_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => calculator.Division(10, 0));
+        }
+
+        [Test]
+        public void AllClear_ResetsResultToZero()
+        {
+            calculator.Addition(5, 5);
+            calculator.AllClear();
+            Assert.AreEqual(0, calculator.GetResult);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            calculator = null;
+        }
     }
 }
-
-
